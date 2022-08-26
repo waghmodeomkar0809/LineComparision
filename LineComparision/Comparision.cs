43cd466c926c0029b8c6a5a35b2acd3c71dd0001
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-08-26 18:48:42 +0530

    add LineComparisionLength

---
+++ LineComparision/Comparision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Comparision
+    {
+        int x1,x2, y1, y2;
+        public Comparision(int _x1, int _x2, int _y1, int _y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public void CalculateLength()
+        {
+            double LineLength = Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2));
+            Console.WriteLine(LineLength);
+        }
+    }
+}
---
+++ LineComparision/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LineComparision;
+
+Comparision comparison = new Comparision(1,2,3,4);
+comparison.CalculateLength();
