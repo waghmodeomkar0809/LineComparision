5b164200498694d687b456fdff45acd50a1edbe5
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-08-27 22:31:55 +0530

    add LineComparisionGreater

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -7,12 +7,16 @@ Console.WriteLine("Length of first line" +line1);
 Comparision LineComparision = new Comparision(1,2,3,4);
 double Line2 = comparison.CalculateLength();
 Console.WriteLine("Length of second line" + Line2);
-if (line1 == Line2)
+if (line1.CompareTo(Line2) == 0)
 {
-    Console.WriteLine("Line1 is equal to Line2");
+    Console.WriteLine("both line are same");
+}
+if (line1.CompareTo(Line2) > 0)
+{
+    Console.WriteLine("line1 is greter than Line2");
 }
 else
 {
-    Console.WriteLine("Line1 is not equals to Line2");
+    Console.WriteLine("Line2 is greter than Line1");
 }
 
